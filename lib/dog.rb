65c85dfc2239a6ae95497909f1e95cb9be0b2b4d
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-16 15:17:19 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def initialize=(dogs_name)
+    @dogs_name=dogs_name
+  end
+  def name
+    @dogs_name
+  end
+  def initialize(breed)
+    @breed=breed
+  end
+  def breed=(breed)
+    @breed=breed
+  end
+  def breed
+    @breed
+  end
+end
